f8983be1a809a6f10f1f628fa817e32622fcd547
Rohithyeravothula <rohithiitj@gmail.com>
2017-04-27 19:48:46 +0530

    adds custom partitioner

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Contains small code snippets usedfull whlie writing code on spark 
+Contains small code snippets used whlie writing code on spark 
--- src/main/scala/CustomPartitionerTest.scala
+++ src/main/scala/CustomPartitionerTest.scala
@@ -5,27 +5,31 @@ import org.apache.spark.{Partitioner, SparkConf, SparkContext}
   */
 
 object CustomPartitionerTest {
-  class CustomPartitioner(num: Int) extends Partitioner {
-
+  class CustomPartitioner(allKeys: List[Any]) extends Partitioner {
+    val allKeysPartitions = allKeys.zipWithIndex.toMap
     // define your custom partition classification method
-    def getPartitionNumber(max: Int, key: Any): Int = {
-      Math.abs(key.hashCode()) % max
+    def getPartitionNumber(key: Any): Int = {
+      allKeysPartitions.get(key) match{
+        case Some(parNum) => parNum
+        case None => 0
+      }
     }
 
-    override def numPartitions: Int = num
+    override def numPartitions: Int = allKeys.length
 
     override def getPartition(key: Any): Int = {
-      val partitionNum = {getPartitionNumber(num, key)}
+      val partitionNum = getPartitionNumber(key)
       println(s"current key : $key belongs to $partitionNum")
       partitionNum
     }
   }
 
-  def main(args: Array[String]): Unit = {
+  def partitionExample(args: Array[String]): Unit = {
     val conf = new SparkConf().setMaster("local[*]").setAppName("test partition")
     val sc = new SparkContext(conf)
-    val rdd = sc.parallelize(1 to 100).repartition(10).keyBy(_ * 2).partitionBy(new CustomPartitioner(20))
-    rdd.saveAsTextFile("data/int")
+    val allKeys = 1 to 20
+    val rdd = sc.parallelize(1 to 20).repartition(10).keyBy(_ * 2).partitionBy(new CustomPartitioner(allKeys.toList))
+    rdd.saveAsTextFile("data/ParInfo")
   }
 }
 
